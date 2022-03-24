d2a1e3fd3560af97076d8a8208d5f46e47a6c509
graholsky <73984463+xoz1986@users.noreply.github.com>
2022-03-24 16:26:17 +0200

    files

---
+++ sourcefiles/mysql/dump/havotdaa_dbclinic.sql
@@ -0,0 +1,9 @@
+-- Adminer 4.8.1 MySQL 5.6.28-0ubuntu0.14.04.1 dump
+
+CREATE DATABASE IF NOT EXISTS havotdaa_dbclinic;
+USE havotdaa_dbclinic;
+
+CREATE USER 'havotdaa_havot'@'localhost' IDENTIFIED BY 'clinica8565';
+GRANT ALL PRIVILEGES ON havotdaa_dbclinic TO 'havotdaa_havot'@'localhost';
+
+-- 2022-03-18 11:37:29
